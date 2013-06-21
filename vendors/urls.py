8655105348ac69c4c7c46d8e091769b8d0f936d5
HanielDorton <danhorton0@gmail.com>
2013-06-21 12:29:59 -0700

    static

--- vendors/urls.py
+++ vendors/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
 	(r'^logout/$', 'furniture.views.logout_view'),
 	(r'^MFC_view/$', 'furniture.views.mfc_view'),
 	(r'^customer_view/$', 'furniture.views.customer_view'),
-	
+	(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 	
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -27,6 +27,5 @@ urlpatterns = patterns('',
 	 url(r'^autocomplete/', include('autocomplete_light.urls')),
 	 
 	 
-	(r’^static/(?P.*)$’, ‘django.views.static.serve’, {‘document_root’: settings.STATIC_ROOT}).
 )
 
