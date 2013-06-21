0ac4df6ba952109170e2dbf44a474bf53f459055
HanielDorton <danhorton0@gmail.com>
2013-06-21 12:17:54 -0700

    static

--- vendors/settings.py
+++ vendors/settings.py
@@ -61,7 +61,7 @@ STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 #STATIC_URL = 'http://danielhorton.site11.com/static/'
-STATIC_URL = '/furniture/staticfiles/')
+STATIC_URL = os.path.join(PROJECT_ROOT,'static_url/')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
