e10122c085215c3c62263f1a6166c8251f319044
HanielDorton <danhorton0@gmail.com>
2014-02-14 12:56:16 -0800

    Live

--- manage.py
+++ manage.py
@@ -5,9 +5,9 @@ import sys
 if __name__ == "__main__":
 
 	#LIVE:
-	#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vendors.settings")
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vendors.settings")
 	#TEST: (uses static files form heroku server still )
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vendors.TESTsettings")
+	#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vendors.TESTsettings")
 
 	from django.core.management import execute_from_command_line
 	execute_from_command_line(sys.argv)
